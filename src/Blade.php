3bffbc172b7ba35dabab9c86d56ae980311225ab
ROBROY BUSTILLO CANALES <canales.robroy123@gmail.com>
2025-06-05 17:48:06 +0800

    just remove the file created

--- src/Blade.php
+++ src/Blade.php
@@ -133,7 +133,7 @@
 		 * Errors during execution are caught and formatted into a detailed HTML error message,
 		 * including stack traces, then thrown as a CompilerException.
 		 *
-		 * The temporary compiled file is deleted after execution, except in development mode.
+		 * The temporary compiled file is deleted after execution.
 		 *
 		 * @param string $content The compiled PHP content to execute.
 		 * @param array $data An associative array of variables to be extracted into the scope of the included template.
@@ -144,8 +144,7 @@
 		{
 			static $errorTraces = [];
 
-			$development = defined('DEVELOPMENT') && DEVELOPMENT;
-			$tempFile = $development ? '../blade.php' : tempnam(sys_get_temp_dir(), 'tpl_') . '.php';
+			$tempFile = tempnam(sys_get_temp_dir(), 'tpl_') . '.php';
 			try
 			{
 				file_put_contents($tempFile, $content);
@@ -168,7 +167,7 @@
 					];
 				}
 			} finally {
-				if (!$development) unlink($tempFile);
+				unlink($tempFile);
 			}
 
 			if (!empty($errorTraces)) {
