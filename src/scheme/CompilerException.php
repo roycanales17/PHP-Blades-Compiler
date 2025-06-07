816c1fbbc79cce65cc1a52bc442da1e3bf2f1c4d
ROBROY BUSTILLO CANALES <canales.robroy123@gmail.com>
2025-06-07 09:41:21 +0800

    add 500 http code

--- src/scheme/CompilerException.php
+++ src/scheme/CompilerException.php
@@ -18,6 +18,7 @@
 		 */
 		public function __construct(string $message = "", int $code = 0, ?\Throwable $previous = null)
 		{
+			http_response_code(500);
 			parent::__construct($message, $code, $previous);
 		}
 
