e87c9e6c755843ea62533eae52031d6d00071081
ROBROY BUSTILLO CANALES <canales.robroy123@gmail.com>
2025-11-18 22:59:25 +0800

    feat: new wrapper for error tags

--- directives/__4_Tags.php
+++ directives/__4_Tags.php
@@ -1,5 +1,6 @@
 <?php
 
+	use App\Utilities\Session;
 	use App\View\Compilers\Blade;
 	use App\View\Compilers\templates\Tags;
 
@@ -20,6 +21,24 @@
 		$blade->wrap('@php', '@endphp', function ($expression) {
 			return "<?php $expression ?>";
 		});
+
+		$blade->wrap('@error', '@enderror', function ($expression, $key) {
+			if (class_exists(Session::class)) {
+				$message = Session::flash($key);
+
+				// Insert again
+				Session::flash("error:$key", $message);
+				if ($message) {
+					return "<?php if (true): ?>
+								$expression
+							<?php endif; ?>";
+				}
+			} else {
+				// Todo: Will add the legacy logic here...
+			}
+
+			return "";
+		}, true);
 
 		$blade->wrap('@section', '@endsection', function ($expression, $param) use($blade) {
 			if (!isset($GLOBALS['__BLADE_YIELD__'])) {
